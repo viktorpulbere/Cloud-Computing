0a92ede5642c023cb76d22b47bfefd1536e841c2
Victor PULBERE <vpulbere@bitdefender.com>
2020-03-15 15:47:34 +0200

    List devices endpoint

--- tema2/public/routes/devices.js
+++ tema2/public/routes/devices.js
@@ -6,6 +6,7 @@ const schema = require('../../schema');
 
 const { ObjectID } = require('mongodb');
 const tv4 = require('tv4');
+const _ = require('lodash');
 
 const router = new Router();
 
@@ -29,6 +30,8 @@ router.post('/users/:userId/devices', async (req, res) => {
         }
         
         deviceInfo.userId = userId;
+        deviceInfo.lastUpdate = Date.now();
+
         const { insertedId: deviceId } = await shared.mongo.devices.insertOne(deviceInfo);
 
         res.json({ deviceId }, 201);
@@ -130,7 +133,10 @@ router.patch('/users/:userId/devices/:deviceId', async (req, res) => {
             userId, 
             _id: ObjectID(deviceId) 
         }, { 
-            $set: updateExpression 
+            $set: { 
+                ... updateExpression,
+                lastUpdate: Date.now()
+            } 
         });
 
         if (!result.matchedCount) {
@@ -174,12 +180,73 @@ router.patch('/users/:userId/devices', async (req, res) => {
             }, 404);
         }
         
-        const result = await shared.mongo.devices.updateMany({ userId }, { $set: updateExpression });
+        const result = await shared.mongo.devices.updateMany({ userId }, { 
+            $set: { 
+                ... updateExpression,
+                lastUpdate: Date.now()
+            }  
+        });
 
         res.json({
             matched: result.matchedCount,
             modified: result.modifiedCount 
         }, 200);
+    } catch (err) {
+        res.json({
+            message: 'Internal Server Error'
+        }, 500);
+    }
+});
+
+router.get('/users/:userId/devices', async (req, res) => {
+    try {
+        const querystring = req.querystring;
+        const { userId } = req.params;
+        const query = {};
+        const page = parseInt(
+            _.get(querystring, 'page', 1)
+        );
+        const status = parseInt(
+            _.get(querystring, 'status', -1)
+        );
+        let pageLimit = parseInt(
+            _.get(querystring, 'limit', 10)
+        );
+        const type = _.get(querystring, 'type', null);
+
+        if (page < 0 || pageLimit < 0) {
+            return res.json({
+                message: 'Bad request'
+            }, 400);
+        }
+
+        if (pageLimit > 100) {
+            pageLimit = 100;
+        }
+
+        const user = await shared.mongo.users.findOne({ _id: ObjectID(userId) });
+        
+        if (user === null) {
+            return res.json({ 
+                message: 'User not found' 
+            }, 404);
+        }
+
+        if (status != -1) {
+            query.status = status;
+        }
+
+        if (type) {
+            query.type = type;
+        }
+
+        const devices = await shared.mongo.devices
+            .find(query, { projection: { _id: 0, userId: 0, lastUpdate: 0 } })
+            .skip(pageLimit * (page - 1))
+            .limit(pageLimit)
+            .toArray();
+            
+        res.json(devices, 200);
     } catch (err) {
         res.json({
             message: 'Internal Server Error'
