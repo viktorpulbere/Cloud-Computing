547de292e89b516c53007c69daa4801e695d51b6
Victor PULBERE <vpulbere@bitdefender.com>
2020-04-04 12:48:17 +0300

    Removed config.ts

--- open-trade/frontend/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    // apiUrl: 'https://cchomework-272510.appspot.com',
-    apiUrl: 'https://plucky-point-272511.ey.r.appspot.com/',
-    apiKey: 'AIzaSyABI62zN62V-GKBX0lGcfcWQjxNs5XM_tc',
-    geocodeUrl: 'https://maps.googleapis.com/maps/api/geocode/json'
-};
